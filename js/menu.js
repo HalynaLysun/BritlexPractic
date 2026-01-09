a5c0e9717d700d56f39a0b789d7f47091879486c
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2026-01-09 18:11:21 +0100

    debuging burger-menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,21 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-    };
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    menuLinks: document.querySelectorAll(".menu-link"),
+  };
 
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-    }
-  })();
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+
+  refs.menuLinks.forEach((link) => link.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    refs.menu.classList.add("is-hidden");
+  }
+})();
